4e15e88f723ed30dc7c44f01310e80d440bec62b
Greg Hesp <768908+greghesp@users.noreply.github.com>
2025-04-20 20:32:03 +0100

    Printer fix (#56)
    * Update const.ts
    
    * Update const.ts
    
    * Update const.ts
    
    Added AMS 2 Pro

--- src/cards/print-control-card/const.ts
+++ src/cards/print-control-card/const.ts
@@ -4,4 +4,4 @@ export const PRINT_CONTROL_CARD_NAME = `${PREFIX_NAME}-print_control-card`;
 export const PRINT_CONTROL_CARD_NAME_LEGACY = `${PREFIX_NAME}-skipobject-card`;
 export const PRINT_CONTROL_CARD_EDITOR_NAME = `${PRINT_CONTROL_CARD_NAME}-editor`;
 export const PRINT_CONTROL_CARD_ENTITY_DOMAINS = ["bambu_lab"];
-export const PRINTER_MODELS = ["A1", "A1 MINI", "A1 Mini", "A1MINI", "A1Mini", "A1mini", "P1P", "P1S", "X1C", "X1E"];
+export const PRINTER_MODELS = ["A1", "A1 MINI", "A1 Mini", "A1MINI", "A1Mini", "A1mini", "P1P", "P1S", "X1C", "X1E", "H2D"];
