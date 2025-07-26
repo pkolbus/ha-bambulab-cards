74e3658570cefc7a55a6a0f8b8c0c386e6d3d4ad
AdrianGarside <adriangarside@outlook.com>
2025-07-26 00:46:38 -0700

    Fix remaining time on graphical card (#94)

--- src/cards/print-status-card/print-status-card.ts
+++ src/cards/print-status-card/print-status-card.ts
@@ -495,7 +495,7 @@ export class PrintStatusCard extends EntityProvider {
 
         case "remaining_time":
           return html` <div id="${key}" class="entity" style="${style}">
-            ${helpers.formatMinutes(Number(text))}
+            ${helpers.getFormattedEntityState(this._hass, this._deviceEntities['remaining_time'].entity_id)}
           </div>`;
 
         case "door_open":
